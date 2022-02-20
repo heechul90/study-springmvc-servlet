1fd2c465a4ed3a4a3db97409be2e580b20f2ae7e
hclee <heechul4296@gmail.com>
2022-02-20 19:27:21 +0900

    model 추가 - v3

---
+++ src/main/java/hellcoding/spring/servlet/web/frontController/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,24 @@
+package hellcoding.spring.servlet.web.frontController.v3.controller;
+
+import hellcoding.spring.servlet.domain.member.Member;
+import hellcoding.spring.servlet.domain.member.MemberRepository;
+import hellcoding.spring.servlet.web.frontController.ModelView;
+import hellcoding.spring.servlet.web.frontController.v3.ControllerV3;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+
+        List<Member> members = memberRepository.findAll();
+        ModelView mv = new ModelView("members");
+        mv.getModel().put("members", members);
+        return mv;
+
+    }
+}
